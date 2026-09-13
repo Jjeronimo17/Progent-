3e239a6ab4d10c86baf69b1c6aaad02ca0d49bbf
Jjeronimo17 <jjaramila1@eafit.edu.co>
2026-09-13 12:53:55 -0500

    Agregado funcion hashing con password Salted

--- Entrega#2/login.py
+++ Entrega#2/login.py
@@ -4,7 +4,28 @@ import random
 import string
 
 
+usuario = ""
+password = ""
+
+
 def generarSalt():
     longitud = 15
     salt = "".join(random.choices(string.ascii_letters + string.digits, k=15))
-    print(salt)
+    return salt
+
+
+def hashing(password):
+    password = input()
+    salt = generarSalt()
+    mitadSalt = len(salt) // 2
+    primeraMitadSalt = salt[: mitadSalt + 1]
+    segundaMitadSalt = salt[mitadSalt + 1 :]
+
+    mitadPass = len(password) // 2
+    primeraMitadPass = password[: mitadPass + 1]
+    segundaMitadPass = password[mitadPass + 1 :]
+    passwordcomplete = (
+        primeraMitadPass + primeraMitadSalt + segundaMitadPass + segundaMitadSalt
+    )
+    passwordHashed = hash(passwordcomplete)
+    return passwordHashed
