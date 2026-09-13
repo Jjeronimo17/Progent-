d773184537f1d7932dde00ea91266bbbc5ac9032
Jjeronimo17 <jjaramila1@eafit.edu.co>
2026-09-13 15:34:40 -0500

    Funcion generadora de Salt cambiada, funcion de Hashing cambiada. Guardar usuarios nuevos en la base de datos añadido
    La funcion de Salt era generada por la libreria random, se cambio por la libreria secrets para mayor robustez, la funcion de hashing utilizaba la funcion hash() de la libreria estandar, en pruebas se descrubrio que la naturaleza de esta funcion cambia el hash en cada ejecucion del interprete incluso con entradas iguales, se utilizo la libreria hashlib y pbkdf2 a unas 600000 iteraciones para mayor robustez y añadir una capa de seguridad al retrasar la cantidad de intentos que los atacantes pueden hacer para intentar decifrar el hash

--- Entrega#2/login.py
+++ Entrega#2/login.py
@@ -1,31 +1,67 @@
 import mysql.connector
 from mysql.connector import Error
-import random
-import string
+import secrets
+import hashlib
+
+CANTIDAD_ITERACIONES = 600000
 
 
 usuario = ""
-password = ""
+passwordUser = ""
 
 
 def generarSalt():
-    longitud = 15
-    salt = "".join(random.choices(string.ascii_letters + string.digits, k=15))
+    salt = secrets.token_hex(16)
     return salt
 
 
-def hashing(password):
-    password = input()
+def hashing(passwordUser):
     salt = generarSalt()
-    mitadSalt = len(salt) // 2
-    primeraMitadSalt = salt[: mitadSalt + 1]
-    segundaMitadSalt = salt[mitadSalt + 1 :]
-
-    mitadPass = len(password) // 2
-    primeraMitadPass = password[: mitadPass + 1]
-    segundaMitadPass = password[mitadPass + 1 :]
-    passwordcomplete = (
-        primeraMitadPass + primeraMitadSalt + segundaMitadPass + segundaMitadSalt
+    saltBytes = salt.encode()
+    passwordUserBytes = passwordUser.encode()
+    passwordHashedBytes = hashlib.pbkdf2_hmac(
+        "sha256", passwordUserBytes, saltBytes, CANTIDAD_ITERACIONES
     )
-    passwordHashed = hash(passwordcomplete)
-    return passwordHashed
+    passwordHashed = passwordHashedBytes.hex()
+    return passwordHashed, salt
+
+
+def guardarDataBase(usuario, passwordUser):
+    usuario = input("Ingrese un nombre de usuario: ")
+    passwordUser = input("Ingrese una contraseña: ")
+    passwordHash, salt = hashing(passwordUser)
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            database="progent_usuarios",
+            user="root",
+            password="20081996Jeronimo13",
+        )
+        if conexion.is_connected():
+            cursor = conexion.cursor()
+            atributos = (usuario, "")
+            resultado = cursor.callproc("verificar_usuario", atributos)
+            verificacion = resultado[1]
+            if verificacion == "Usuario No Disponible":
+                print("Usuario no disponible, ingrese otro usuario")
+            else:
+                sql_insertar = (
+                    "INSERT INTO usuarios(Usuario, Salt, Hashed) VALUES (%s, %s, %s)"
+                )
+                datos = (usuario, salt, passwordHash)
+                cursor.execute(sql_insertar, datos)
+                conexion.commit()
+                print("Usuario Creado con exito")
+
+    except Error as e:
+        print(f"Error durante las operaciones {e}")
+
+    finally:
+        if "conexion" in locals() and conexion.is_connected():
+            cursor.close()
+            conexion.close()
+            print("Conexion Cerrada")
+
+
+guardarDataBase(usuario, passwordUser)
