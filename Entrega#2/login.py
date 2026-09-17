b9b7dd4864e9da1cef9f53cc86511c5bf6a011a2
Jjeronimo17 <jjaramila1@eafit.edu.co>
2026-09-16 21:36:44 -0500

    Funcion de iniciar sesion creada
    Login finalizado

--- Entrega#2/login.py
+++ Entrega#2/login.py
@@ -7,10 +7,6 @@ import re
 CANTIDAD_ITERACIONES = 600000
 
 
-correo = ""
-passwordUser = ""
-
-
 def generarSalt():
     salt = secrets.token_hex(16)
     return salt
@@ -28,8 +24,7 @@ def hashing(passwordUser):
 
 
 def guardarDataBase(correo, passwordUser):
-    correo = input("Ingrese un nombre de usuario: ")
-    passwordUser = input("Ingrese una contraseña: ")
+    correo = correo.lower()
     passwordHash, salt = hashing(passwordUser)
     try:
         conexion = mysql.connector.connect(
@@ -47,9 +42,9 @@ def guardarDataBase(correo, passwordUser):
             miRegex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
             resultado = re.search(miRegex, correo)
             if verificacion == "Usuario No Disponible":
-                print("Correo asociado a una cuenta, use otro")
+                return False, "Correo asociado a una cuenta, use otro"
             elif not (resultado):
-                print("Ingrese una direccion de correo valida")
+                return False, "Ingrese una direccion de correo valida"
             else:
                 sql_insertar = (
                     "INSERT INTO usuarios(Correos, Salt, Hashed) VALUES (%s, %s, %s)"
@@ -57,16 +52,52 @@ def guardarDataBase(correo, passwordUser):
                 datos = (correo, salt, passwordHash)
                 cursor.execute(sql_insertar, datos)
                 conexion.commit()
-                print("Usuario Creado con exito")
+                return True, "Registro exitoso"
 
     except Error as e:
         print(f"Error durante las operaciones {e}")
+        return False, "No se pudo completar el registro. Intenta de nuevo."
 
     finally:
         if "conexion" in locals() and conexion.is_connected():
             cursor.close()
             conexion.close()
-            print("Conexion Cerrada")
 
 
-guardarDataBase(correo, passwordUser)
+def iniciarSesion(correo, passwordUser):
+    passwordCorrecto = False
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            database="progent_usuarios",
+            user="root",
+            password="20081996Jeronimo13",
+        )
+        if conexion.is_connected():
+            cursor = conexion.cursor()
+            correo_buscar = correo
+            sql_selectSalt = "SELECT SALT FROM Usuarios WHERE Correos = %s"
+            cursor.execute(sql_selectSalt, (correo_buscar,))
+            saltTuple = cursor.fetchone()
+            salt = saltTuple[0]
+            sql_selectHash = "SELECT Hashed FROM Usuarios WHERE Correos = %s"
+            cursor.execute(sql_selectHash, (correo_buscar,))
+            hashedTuple = cursor.fetchone()
+            hashed = hashedTuple[0]
+            saltBytes = salt.encode()
+            passwordUserBytes = passwordUser.encode()
+            passwordVerificarBytes = hashlib.pbkdf2_hmac(
+                "sha256", passwordUserBytes, saltBytes, CANTIDAD_ITERACIONES
+            )
+            passwordVerificar = passwordVerificarBytes.hex()
+            verificacion = secrets.compare_digest(passwordVerificar, hashed)
+            if verificacion:
+                passwordCorrecto = True
+                return passwordCorrecto
+    except Error as e:
+        print(f"Error durante las operaciones {e}")
+    finally:
+        if "conexion" in locals() and conexion.is_connected():
+            cursor.close()
+            conexion.close()
